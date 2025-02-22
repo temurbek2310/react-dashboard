5540cb8047a89dad36682d4fb14cb6cf7ec77c31
temurbek2310 <ghostwire2010@gmail.com>
2025-02-22 19:22:31 +0500

    added protected route

--- src/routes/react-router.jsx
+++ src/routes/react-router.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/home";
 import { NotFound } from "../pages/not-found";
+import { Login } from "../pages/login";
 
 const router = createBrowserRouter([
   {
+    path: "/login",
+    Component: Login,
+  },
+  {
     path: "/",
+    index: true,
     Component: Home,
   },
   {
